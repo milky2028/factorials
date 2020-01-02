30c575a06c1259947be1ebb9b0e0d3f8fa96e28a
Tyler <tgross@skyroad.com>
2020-01-02 09:36:50 -0600

    formatter

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 bool isPrime(size_t n) {
-    for (size_t i = 2; i < n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
+  for (size_t i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 void findPrimes(size_t n, size_t primes[]) {
-    primes[0] = 0; 
-    primes[1] = 1;
-    for (size_t i = 2; i <= n; i++) {
-        if (isPrime(i)) {
-            primes[i] = i;
-        } else {
-            primes[i] = 0;
-        }
+  primes[0] = 0;
+  primes[1] = 1;
+  for (size_t i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      primes[i] = i;
+    } else {
+      primes[i] = 0;
     }
+  }
 }
 
 // void findPrimeFactorials(size_t n, size_t primes[], size_t factorials[]) {
@@ -36,16 +36,16 @@ void findPrimes(size_t n, size_t primes[]) {
 // }
 
 int main(int argc, char* argv[argc + 1]) {
-    size_t n = (size_t)atoi(argv[1]) + 1;
-    size_t primeNumbers[n];
-    findPrimes(n, primeNumbers);
+  size_t n = (size_t)atoi(argv[1]) + 1;
+  size_t primeNumbers[n];
+  findPrimes(n, primeNumbers);
 
-    // size_t factorials[n];
-    // findPrimeFactorials(n, primeNumbers, factorials);
+  // size_t factorials[n];
+  // findPrimeFactorials(n, primeNumbers, factorials);
 
-    for (size_t i = 0; i < n; i++) {
-        printf("%lu\n", primeNumbers[i]);
-    }
+  for (size_t i = 0; i < n; i++) {
+    printf("%lu\n", primeNumbers[i]);
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
