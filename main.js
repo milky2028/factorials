b19959e2bc4302c049c01a9f9b521a02868ffd1a
Tyler <tgross@skyroad.com>
2020-01-08 12:45:18 -0600

    make this more concise

--- main.js
+++ main.js
@@ -2,24 +2,20 @@ const c = require('./f.js');
 
 c().then(({ _findPrimes, _getData, _getLen, _getUnitSize, HEAPU32 }) => {
   class Primes {
-    constructor(ptr) {
-      this.data = _getData(ptr);
-      this.len = _getLen(ptr);
-      this.unitSize = _getUnitSize(ptr);
+    constructor(ptr, memory) {
+      this.memory = memory;
+      this.start = _getData(ptr) / _getUnitSize(ptr);
+      this.end = this.start + _getLen(ptr);
     }
 
-    getStart() {
-      return this.data / this.unitSize;
-    }
-
-    getEnd() {
-      return this.getStart() + this.len;
+    data() {
+      return this.memory.subarray(this.start, this.end);
     }
   }
 
-  const primesPtr = _findPrimes(100);
-  const primes = new Primes(primesPtr);
-  const primeNumbers = HEAPU32.subarray(primes.getStart(), primes.getEnd());
+  const ptr = _findPrimes(7);
+  const primes = new Primes(ptr, HEAPU32);
+  const primeNumbers = primes.data();
   console.log(primeNumbers);
 });
 
