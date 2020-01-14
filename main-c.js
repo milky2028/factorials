25aa088904ef7dbfe613a48bf2112d5a132377ee
Tyler <tgross@skyroad.com>
2020-01-14 14:06:56 -0600

    dd benchmarking

--- main-c.js
+++ main-c.js
@@ -4,21 +4,45 @@ if (!max) {
   throw new Error('Error: no arguments specified. Please specify an upper bound.');
 }
 
-c().then(({ _findPrimes, _getData, _getLen, _getUnitSize, HEAPU32 }) => {
-  class Primes {
-    constructor(ptr, memory) {
-      this.memory = memory;
-      this.start = _getData(ptr) / _getUnitSize(ptr);
-      this.end = this.start + _getLen(ptr);
-    }
+process.setMaxListeners(Infinity);
 
-    data() {
-      return this.memory.subarray(this.start, this.end);
-    }
-  }
+function findPrimesInC() {
+  const start = Date.now();
+  return new Promise((resolve) => {
+    c().then(({ _findPrimes, _getData, _getLen, _getUnitSize, HEAPU32 }) => {
+      class Primes {
+        constructor(ptr, memory) {
+          this.memory = memory;
+          this.start = _getData(ptr) / _getUnitSize(ptr);
+          this.end = this.start + _getLen(ptr);
+        }
 
-  const ptr = _findPrimes(max);
-  const primes = new Primes(ptr, HEAPU32);
-  const primeNumbers = primes.data();
-  console.log(primeNumbers);
-});
+        data() {
+          return this.memory.subarray(this.start, this.end);
+        }
+      }
+
+      const ptr = _findPrimes(max);
+      const primes = new Primes(ptr, HEAPU32);
+      const primeNumbers = primes.data();
+      resolve(Date.now() - start);
+    });
+  });
+}
+
+async function main() {
+  const iterations = [...Array(20).keys()];
+  const results = await Promise.all(iterations.map(async () => await findPrimesInC()));
+  const avg = results.reduce((a, c) => a + c, 0) / iterations.length;
+  return avg;
+}
+
+main()
+  .then((res) => {
+    console.log(res);
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
