cdf3299b0a9216758ae48d31c5f47ce3dc3aac53
Tyler <tylergross28@gmail.com>
2020-02-16 23:19:59 -0600

    ptr is unecessary

--- cc/primes.h
+++ cc/primes.h
@@ -5,12 +5,6 @@
 
 using std::vector;
 
-typedef struct UInt32Array {
-  vector<uint32_t> data;
-  size_t len;
-  size_t unit_size;
-} UInt32Array;
-
 bool is_prime(size_t n) {
   for (size_t i = 2; i < sqrt(n); ++i) {
     if (n % i == 0) {
@@ -21,7 +15,7 @@ bool is_prime(size_t n) {
   return n != 0 && n != 1;
 }
 
-UInt32Array* find_primes(size_t max) {
+vector<uint32_t> find_primes(size_t max) {
   vector<uint32_t> primes;
   for (size_t i = 2; i <= max; ++i) {
     if (is_prime(i)) {
@@ -29,10 +23,5 @@ UInt32Array* find_primes(size_t max) {
     }
   }
 
-  UInt32Array* ptr =
-      (UInt32Array*)malloc(primes.size() * sizeof(uint32_t) + sizeof(size_t) * 2);
-  ptr->data = primes;
-  ptr->len = primes.size();
-  ptr->unit_size = sizeof(primes[0]);
-  return ptr;
+  return primes;
 }
