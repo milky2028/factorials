04d04788a8d0699fd56ed1bba1d0419cbe5ddd18
Tyler Gross <tylergross28@gmail.com>
2020-03-09 18:06:38 +0000

    proper exception handling

--- cc/pure/main.cc
+++ cc/pure/main.cc
@@ -1,11 +1,13 @@
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include "../primes.h"
 
 using std::cout;
 using std::endl;
 using std::exit;
+using std::invalid_argument;
 using std::stoul;
 using std::string;
 using std::vector;
@@ -14,25 +16,24 @@ void print_bounded_error(const string msg) {
   cout << "\n===============================================================\n";
   cout << msg << endl;
   cout << "===============================================================\n\n";
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char* argv[]) {
   if (argc == 1) {
     print_bounded_error(
         "Error: no arguments specified. Please specify an upper bound.");
-    exit(EXIT_FAILURE);
   };
 
-  // TODO: This exception is not properly handled
-  size_t max = stoul(argv[1]);
-  if (!max) {
-    print_bounded_error("Please specify a valid number.");
-    exit(EXIT_FAILURE);
-  }
+  try {
+    uint32_t max = stoul(argv[1]);
 
-  auto primes = find_primes(max);
-  for (auto& prime_number : primes) {
-    cout << prime_number << endl;
+    auto primes = find_primes(max);
+    for (auto& prime_number : primes) {
+      cout << prime_number << endl;
+    }
+  } catch (invalid_argument e) {
+    print_bounded_error("Please specify a valid number.");
   }
 
   return EXIT_SUCCESS;
