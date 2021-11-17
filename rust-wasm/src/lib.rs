023e1b99e21b88901fce01142155b16a86631f21
Tyler Gross <tylergross28@gmail.com>
2021-11-17 11:04:56 -0500

    not externally C functions

--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -5,12 +5,12 @@ mod primes;
 
 #[wasm_bindgen]
 #[no_mangle]
-pub extern "C" fn find_primes_wasm(max: u32) -> Vec<u32> {
+pub fn find_primes_wasm(max: u32) -> Vec<u32> {
     primes::find_primes(max)
 }
 
 #[wasm_bindgen]
 #[no_mangle]
-pub extern "C" fn test_primes(primes_to_test: Vec<u32>) -> bool {
+pub fn test_primes(primes_to_test: Vec<u32>) -> bool {
     primes_to_test.into_iter().all(|i| primes::is_prime(i))
 }
